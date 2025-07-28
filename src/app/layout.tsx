6b49eb9597a591e86644d3c494564aa58901da66
kuroka3 <kuroka357@gmail.com>
2025-07-29 05:07:37 +0900

    Change favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "ARONA",
   description: "ARONA Official Website",
+  icons: {
+    icon: "favicon.ico",
+  },
 };
 
 export default function RootLayout({
