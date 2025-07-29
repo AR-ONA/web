1c757339a319ffad55c06488d27839e6ceedc7ed
kuroka3 <kuroka357@gmail.com>
2025-07-30 03:27:53 +0900

    Layout Issue

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,11 @@
 import { Metadata } from "next";
+import { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "Admin - ARONA",
   description: "Admin Page",
 };
+
+export default function AdminLayout({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
