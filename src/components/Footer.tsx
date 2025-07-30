f6cc260118bc9812b7500205ece6484c78d4d28d
kuroka3 <kuroka357@gmail.com>
2025-07-30 19:50:48 +0900

    Update Informations

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,9 @@ const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <p>© {new Date().getFullYear()} ARONA. All Rights Reserved.</p>
+        <p>© {new Date().getFullYear()} <a href="https://github.com/AR-ONA/web" target="_blank">ARONA</a> powered by <a href="https://vercel.com/" target="_blank">Vercel</a></p>
+        <p>Images of Blue Archive copyrighted by NEXON GAMES & YOSTAR.</p>
+        <p>This website is not affiliated with or endorsed by NEXON GAMES.</p>
         <p>All Roads, One New Answer</p>
       </div>
     </footer>
